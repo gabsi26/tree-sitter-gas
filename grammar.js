913bf07b180adf8395ff353558d6850346638765
gabsi26 <63023569+gabsi26@users.noreply.github.com>
2025-12-16 11:43:52 +0100

    Fix regex for single-line comments in grammar.js

--- grammar.js
+++ grammar.js
@@ -152,7 +152,7 @@ module.exports = grammar({
     register: $ => /%[a-zA-Z][a-zA-Z0-9]*/,
 
     comment: $ => choice(
-      seq("//", repeat(/./), $.eol),
+      /\/\/.*^/,
       seq('/*', repeat(choice(/./, $._eol)), '*/')
     ),
 
